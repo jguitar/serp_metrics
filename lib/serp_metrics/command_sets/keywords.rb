46d2c7f9b5d4962091cb97f13be49c55ba731135
Juan F. Pérez <j.guitar@gmail.com>
2015-06-05 16:46:40 +0200

    add default options to all transactions

--- lib/serp_metrics/command_sets/keywords.rb
+++ lib/serp_metrics/command_sets/keywords.rb
@@ -1,40 +1,40 @@
 module SerpMetrics
   module CommandSets
     class Keywords < Base
-      def add(keyword, engines)
+      def add(keyword, engines, options = {})
         transaction_options = {
           :keyword => keyword,
           :engines => engines
-        }
+        }.merge(options)
         transact(:post, '/keywords/add', transaction_options)
       end
 
-      def remove(keyword_id)
+      def remove(keyword_id, options = {})
         transaction_options = {
           :keyword_id => keyword_id,
-        }
+        }.merge(options)
         transact(:post, '/keywords/delete', transaction_options)
       end
 
-      def check(keyword_id, engine, limit = 10)
+      def check(keyword_id, engine, options = {})
         transaction_options = {
           :keyword_id => keyword_id,
           :engine => engine,
-          :limit => limit
-        }
+          :limit => 10
+        }.merge(options)
         transact(:post, '/keywords/check', transaction_options)
       end
 
-      def serp(check_id, domain = nil)
+      def serp(check_id, options = {})
         transaction_options = {
           :check_id => check_id,
-          :domain => domain
-        }
+          :domain => nil
+        }.merge(options)
         transact(:post, '/keywords/serp', transaction_options)
       end
 
-      def all
-        transact(:post, '/keywords/all')
+      def all(options = {})
+        transact(:post, '/keywords/all', options)
       end
     end
   end
